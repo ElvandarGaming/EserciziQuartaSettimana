8b3e192fcc8a004f4002eb9e57c9a2e6e5bce081
LucaB <LucaB@host.docker.internal>
2021-07-07 23:11:41 +0200

    exception firing

--- EserciziQuartaSettimana/src/eserciziConnessioni/ConnectionHandler.java
+++ EserciziQuartaSettimana/src/eserciziConnessioni/ConnectionHandler.java
@@ -5,13 +5,12 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class ConnectionHandler4 {
+public class ConnectionHandler {
 	private Connection con;
 	private String url;
 	
-	public ConnectionHandler4(String typeDatabase,String address,String port,String databaseName ,String schema,String username,String password ) {
+	public ConnectionHandler(String typeDatabase,String address,String port,String databaseName ,String schema,String username,String password ) {
 		url = String.format("jdbc:%s://%s:%s/%s?currentSchema=%s&user=%s&password=%s",typeDatabase,address,port,databaseName,schema,username,password);
-		System.out.println(url);
 	}
 	
 	public Connection getConnection(){
@@ -21,8 +20,9 @@ public class ConnectionHandler4 {
 			System.out.println("Driver postgreSQL non trovati");
 		}
 		try {
+			if (con==null||con.isClosed()) {
 			this.con = DriverManager.getConnection(this.url);
-			
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 			System.out.println("Url non corretto, Connessione non stabilita");
@@ -33,18 +33,18 @@ public class ConnectionHandler4 {
 	public void closeConnection(){
 		try {
 			this.con.close();
-			this.con = null;
+			//this.con = null;
 		} catch (SQLException e) {
 			e.printStackTrace();
 			System.out.println("Non è stato possibile chiudere la risorsa.");
 		} catch(NullPointerException e) {
-			System.out.println("La risorsa non è stata aperta");
+			System.out.println("La risorsa non è aperta");
 		}
 	}
 	
 	public PreparedStatement getPreparedStatement(String queryTemplate ){
 		PreparedStatement prep = null;
-		try {
+		try {if (con!=null)
 			prep = con.prepareStatement(queryTemplate);
 		} catch (SQLException e) {
 			e.printStackTrace();
--- EserciziQuartaSettimana/src/eserciziConnessioni/StartConnectionHandler.java
+++ EserciziQuartaSettimana/src/eserciziConnessioni/StartConnectionHandler.java
@@ -2,25 +2,38 @@ package eserciziConnessioni;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-
-import Prof.ConnectionHandler;
 
 public class StartConnectionHandler {
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	public static void main(String[] args){
 		Scanner cons = new Scanner(System.in);
-		ConnectionHandler4 x = new ConnectionHandler4("postgresql", "localhost", "5432", "EsercizioGiorno2", "tirocinio", "postgres", "sex");
+		ConnectionHandler x = new ConnectionHandler("postgrsql", "localhost", "5432", "EsercizioGiorno2", "tirocinio", "postgres", "sex");
 		//ConnectionHandler x = new ConnectionHandler("jdbc:postgresql://localhost:5432/EsercizioGiorno2", "tirocinio", "postgres", "sex");
 		Connection con = x.getConnection();
-		x.getConnection();
-		x.getConnection();
+		PreparedStatement y = x.getPreparedStatement("select cognome from tirocinio.tirocinante a join tirocinio.azienda b on a.id_azienda = b.id where b.nome = ?");
+		cons.next();
+		try {
+			y.setString(1, "Microsoft");
+			ResultSet res = y.executeQuery();
+			List<String> cognomi = new ArrayList<>();
+			while (res.next()) {
+				cognomi.add(res.getString("cogome"));
+			}
+			System.out.println(cognomi.toString());
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			x.closeConnection();
+		}
 		
 		//jdbc:postgresql://localhost:5432/EsercizioGiorno2?currentSchema=tirocinio&user=postgres&password=sex
-		x.closeConnection();
-		cons.next();
-		
+
 		
 		
 		
